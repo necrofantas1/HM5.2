5403b704b1dc2ce17c7451450a24d5f09712efa5
Vladyslav <vladchershev@gmail.com>
2025-06-16 20:27:51 +0200

    changed while

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
-﻿using System.ComponentModel;
+﻿{
+    bool continueProgram = true;
 
-while (true)
-
-{
-    Console.WriteLine("Please enter a number:");
-    string userInput = Console.ReadLine();
-
+    while (continueProgram) //p.s тут був true :)
     {
+        Console.WriteLine("Please enter your number:");
+        string userInput = Console.ReadLine();
+
         try
         {
             int number = Convert.ToInt32(userInput);
-            Console.WriteLine($"\nYou entered the number: {number}");
+            Console.WriteLine($"You entered the number: {number}");
 
-            break;
+            continueProgram = false;
         }
 
         catch (FormatException)
         {
-            Console.WriteLine("The input was not in a correct format. Please enter a valid number.");
+            Console.WriteLine("\nInvalid input. Please enter a valid number.");
         }
         catch (OverflowException)
         {
-            Console.WriteLine("\nThe number is too large or too small for an Int32. Please enter a valid number.");
+            Console.WriteLine("\nThe number you entered is too large or too small. Please try again.");
         }
 
     }
 
 }
+            
+        
