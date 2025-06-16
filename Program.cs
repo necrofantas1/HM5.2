3b1d5ddea00cbdbb0eec6772c02ea6b2be202cbf
Vladyslav <vladchershev@gmail.com>
2025-06-16 17:52:14 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+
+{
+    Console.WriteLine("Please enter a number:");
+    string userInput = Console.ReadLine();
+
+
+    try
+    {
+     int number = Convert.ToInt32(userInput);
+    Console.WriteLine($"\nYou entered the number: {number}");
+
+    }
+
+    catch (FormatException)
+    {
+        Console.WriteLine("The input was not in a correct format. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("\nThe number is too large or too small for an Int32. Please enter a valid number.");
+    }
+
+}
+
+
