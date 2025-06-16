bf024bb92a03cc6acc1b2cf7bf385ac5b972c970
Vladyslav <vladchershev@gmail.com>
2025-06-16 19:00:27 +0200

    added break

--- Program.cs
+++ Program.cs
@@ -1,26 +1,29 @@
-﻿while (true)
+﻿using System.ComponentModel;
+
+while (true)
 
 {
     Console.WriteLine("Please enter a number:");
     string userInput = Console.ReadLine();
 
-
-    try
     {
-     int number = Convert.ToInt32(userInput);
-    Console.WriteLine($"\nYou entered the number: {number}");
+        try
+        {
+            int number = Convert.ToInt32(userInput);
+            Console.WriteLine($"\nYou entered the number: {number}");
 
-    }
+            break;
+        }
 
-    catch (FormatException)
-    {
-        Console.WriteLine("The input was not in a correct format. Please enter a valid number.");
-    }
-    catch (OverflowException)
-    {
-        Console.WriteLine("\nThe number is too large or too small for an Int32. Please enter a valid number.");
+        catch (FormatException)
+        {
+            Console.WriteLine("The input was not in a correct format. Please enter a valid number.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("\nThe number is too large or too small for an Int32. Please enter a valid number.");
+        }
+
     }
 
 }
-
-
